4948c5d436235aa370b59c1d3bc9a7cc4350e026
ALowtonQA <81312845+ALowtonQA@users.noreply.github.com>
2021-08-23 11:51:29 +0100

    Update BusinessIdeasMockTest.java

--- src/test/java/com/qa/business/BusinessIdeasMockTest.java
+++ src/test/java/com/qa/business/BusinessIdeasMockTest.java
@@ -14,25 +14,39 @@ import org.junit.Test;
 import com.qa.service.listService;
 
 public class BusinessIdeasMockTest {
-
+	// These are tests for businessClass 
 	@Test
 	public void testingMocks1() {
-		fail("Not yet implemented");
+		// Mock the dependency (listService)
+		
+		// Create a list for the mock to return
+		
+		// Tell the mock to return your list when the target method is called with your desired argument
+		
+		// Instatiate a businessClass Object
+		
+		// Call your target method, with your desired argument (make sure to store the return value appropriately)
+		
+		// Make an appropriate assertion to confirm the test ran successfully 
+		
 	}
 	
 	@Test
 	public void testingMocks2() {
+		// Try testing multiple method calls
 		fail("Not yet implemented");
 	}
 	
 	
 	@Test
 	public void testingMocks3() {
+		// Try verifying target method was actually called, as well as asserting the result is correct.
 		fail("Not yet implemented");
 	}
 	
 	@Test
 	public void testingMocks4() {
+		// Try testing the delete functionality (Hint: verify)
 		fail("Not yet implemented");
 	}
 
