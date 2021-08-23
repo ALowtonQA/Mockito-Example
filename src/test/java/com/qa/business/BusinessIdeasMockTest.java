28f7d607a6d76498982d3a687c5d9218a9f7f33d
ALowtonQA <81312845+ALowtonQA@users.noreply.github.com>
2021-08-23 09:57:49 +0100

    Update BusinessIdeasMockTest.java

--- src/test/java/com/qa/business/BusinessIdeasMockTest.java
+++ src/test/java/com/qa/business/BusinessIdeasMockTest.java
@@ -17,78 +17,23 @@ public class BusinessIdeasMockTest {
 
 	@Test
 	public void testingMocks1() {
-		
-		listService listServiceMock = mock(listService.class);
-		
-		List<String> ideas = Arrays.asList("Chicken soda", "Leather gumshield", "Chicken flavored icecream");
-		
-		when(listServiceMock.retrieveIdeas("Something")).thenReturn(ideas);
-		
-		
-		businessClass businessClass = new businessClass(listServiceMock);
-		
-		List<String> filteredIdeas = businessClass.retrieveChickenIdeas("Something");
-		assertEquals(2, filteredIdeas.size());
+		fail("Not yet implemented");
 	}
 	
 	@Test
 	public void testingMocks2() {
-		
-		listService listServiceMock = mock(listService.class);
-		
-		List<String> ideas1 = Arrays.asList("Chicken soda", "Leather gumshield", "Chicken flavored icecream");
-		List<String> ideas2 = Arrays.asList("Chicken soda");
-		
-		when(listServiceMock.retrieveIdeas("Nothing")).thenReturn(ideas1).thenReturn(ideas2);
-		
-		businessClass businessClass = new businessClass(listServiceMock);
-		
-		List<String> filteredIdeas = businessClass.retrieveChickenIdeas("Nothing");
-		assertEquals(2, filteredIdeas.size());
-		
-		filteredIdeas = businessClass.retrieveChickenIdeas("Nothing");
-		assertEquals(1, filteredIdeas.size());
-		
+		fail("Not yet implemented");
 	}
 	
 	
 	@Test
 	public void testingMocks3() {
-		
-		listService listServiceMock = mock(listService.class);
-		
-		List<String> ideas1 = Arrays.asList("Chicken soda", "Leather gumshield", "Chicken flavored icecream");
-		
-		businessClass businessClass = new businessClass(listServiceMock);
-		
-		//given
-		given(listServiceMock.retrieveIdeas("")).willReturn(ideas1);
-
-		//when
-		List<String> filteredIdeas = businessClass.retrieveChickenIdeas("");
-
-		//then
-		assertThat(filteredIdeas.size(), is(2));
-		verify(listServiceMock, times(1)).retrieveIdeas(anyString());
+		fail("Not yet implemented");
 	}
 	
 	@Test
 	public void testingMocks4() {
-
-		listService listServiceMock = mock(listService.class);
-		List<String> allIdeas = Arrays.asList("Chicken soda", 
-				"Leather gumshield", "Chicken flavored icecream");
-		
-		when(listServiceMock.retrieveIdeas(anyString())).thenReturn(allIdeas);
-
-		businessClass businessClass = new businessClass(listServiceMock);
-		
-		businessClass.deleteIdeasNotRelatedToChicken("Piers");
-		
-		verify(listServiceMock, never()).deleteIdea("Chicken soda");
-		verify(listServiceMock, never()).deleteIdea("Chicken flavored icecream");
-		verify(listServiceMock, times(1)).deleteIdea("Leather gumshield");
-
+		fail("Not yet implemented");
 	}
 
 }
